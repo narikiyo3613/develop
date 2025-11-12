3f9e63fc29c32c03273ade9f0abbe85733893aae
Aoi3222 <2401362@s.asojuku.ac.jp>
2025-11-12 10:19:06 +0900

    Update inquiry.php

--- php/inquiry.php
+++ php/inquiry.php
@@ -7,5 +7,34 @@
 </head>
 <body>
     <h1>お問い合わせ</h1>
+
+     <style>
+        body {
+            font-family: "Meiryo", sans-serif;
+            margin: 40px;
+        }
+
+        label {
+            font-size: 18px;
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        textarea {
+            width: 500px;
+            height: 120px;
+            font-size: 16px;
+            padding: 8px;
+            border: 1px solid #999;
+            border-radius: 4px;
+            resize: both; /* サイズ変更可能 */
+        }
+    </style>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="message">お問合せ内容:</label>
+        <textarea id="message" name="message"></textarea>
+    </form>
 </body>
 </html>
