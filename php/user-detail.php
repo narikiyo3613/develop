be80ad7a3ebbf214e6f0dbe26cfddddfce197b36
aso2401385 <2401385@s.asojuku.ac.jp>
2025-11-19 10:33:20 +0900

    favorite

--- php/user-detail.php
+++ php/user-detail.php
@@ -49,8 +49,8 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
         <div class="side-menu">
             <ul>
                 <li><a href="#">個人の情報</a></li>
-                <li><a href="../favorite.php">お気に入り</a></li>
-                <li><a href="../history.php">購入履歴</a></li>
+                <li><a href="favorite.php">お気に入り</a></li>
+                <li><a href="history.php">購入履歴</a></li>
             </ul>
         </div>
 
