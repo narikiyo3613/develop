b581fdbc65a56b5d0a30db0898909c7cb3a31997
aso2401385 <2401385@s.asojuku.ac.jp>
2025-11-20 00:10:13 +0900

    admin

---
+++ php/admin/dashboard.php
@@ -0,0 +1,20 @@
+<?php require "admin-check.php"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>管理者ダッシュボード</title>
+<link rel="stylesheet" href="../../css/admin.css">
+</head>
+<body>
+
+<h1>管理者ページ</h1>
+
+<div class="menu">
+    <a href="product-list.php">商品管理</a>
+    <a href="user-list.php">ユーザー管理</a>
+    <a href="logout.php" class="logout">ログアウト</a>
+</div>
+
+</body>
+</html>
