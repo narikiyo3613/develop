2783711b61d07ecff2d5f35a9db936e3f448f192
aso2401385 <2401385@s.asojuku.ac.jp>
2025-11-20 23:27:37 +0900

    Update product-add-check.php

--- php/admin/product-add-check.php
+++ php/admin/product-add-check.php
@@ -15,9 +15,23 @@ $desc = $_POST['description'];
 $image = $_FILES['image'];
 
 $filename = $image['name'];
-$save_path = "../image/" . $filename;
 
-move_uploaded_file($image['tmp_name'], $save_path);
+$save_path = "../../image/" . $filename;
+
+if (!is_writable("../../image/")) {
+    echo "imageフォルダに書き込みできません";
+    exit;
+}
+
+if (!move_uploaded_file($image['tmp_name'], $save_path)) {
+    echo "move_uploaded_file失敗: " . $save_path;
+    exit;
+}
+
+echo "成功！保存先: " . $save_path;
+exit;
+
+
 $image_url = "../image/" . $filename;
 
 $sql = "INSERT INTO products(name, price, stock, category, description, image_url)
